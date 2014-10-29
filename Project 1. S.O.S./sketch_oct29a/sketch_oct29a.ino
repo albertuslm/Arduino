0e9ff6ec995a9147bf34a6af3814f475568597ad
Albertus <alopezmartin@paypal.com>
2014-10-29 11:45:09 +0100

    Arduino projects
    Repository with all of my examples (code & hardware configuration) of
    Arduino Projects

---
+++ Project 1. S.O.S./sketch_oct29a/sketch_oct29a.ino
@@ -0,0 +1,63 @@
+/*
+  S.O.S. Morse Code
+  -----------------
+  Level: Basic
+  Hardware:
+        - Breadboard
+        - 5mm LED
+        - 100omh resistor
+        - x2 jumper wires
+  How to connect:
+        (url to screenshot)
+ 
+  Desc: Using a simple LED and Arduino board, this program makes the LED displays the S.O.S. message in Morse code.
+*/
+
+//LED connected to PIN 7
+int ledPin = 7;
+
+//setup
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+//loop
+void loop()
+{
+  //displaying letter S = . . .
+  for (int i=0; i<3; i++)
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(200);
+    digitalWrite(ledPin,LOW);
+    delay(150);
+  }
+
+  //waiting bettween letters like happens in real communication
+  delay(250);
+  
+  //displaying letter O = - - - 
+  for(int i=0; i<3; i++)
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(500);
+    digitalWrite(ledPin,LOW);
+    delay(150);
+  }
+  
+  //waiting bettween letters like happens in real communication
+  delay(250);
+  
+  //displaying letter S = . . .
+  for (int i=0; i<3; i++)
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(200);
+    digitalWrite(ledPin,LOW);
+    delay(150);
+  }
+  
+  delay(500);
+
+}
